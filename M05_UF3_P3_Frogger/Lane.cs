07c888c53edc4a7182cc54ad16de7240cbe2ec6f
OtherAKIRA <andreas.christian.brun@gmail.com>
2023-05-06 00:37:26 +0200

    mapa

--- M05_UF3_P3_Frogger/Lane.cs
+++ M05_UF3_P3_Frogger/Lane.cs
@@ -6,23 +6,24 @@ namespace M05_UF3_P3_Frogger
 {
     public class Lane
     {
-        public readonly int posY;
-        public readonly int speedElements;
-        public readonly bool speedPlayer;
-        public readonly ConsoleColor background;
-        public readonly bool damageElements;
-        public readonly bool damageBackground;
-        public List<DynamicElement> elements { get; protected set; } = new List<DynamicElement>();
+        public readonly int posY;   // indica la posicion vertical del carril
+        public readonly int speedElements;  // indica la velocidad de los elementos dinamicos que se mueven
+        public readonly bool speedPlayer;   // indica si se mueve con el elemento o no
+        public readonly ConsoleColor background;    // indica el color de fondo
+        public readonly bool damageElements;    // indica si los elementos del carril pueden dañar al jugador
+        public readonly bool damageBackground;  // indica si el fondo del carril puede dañar al jugador
+        public List<DynamicElement> elements { get; protected set; } = new List<DynamicElement>();  // lista de elementos dinamicos que se mueven el carril
 
 
         public Lane(int posY, bool speedPlayer, ConsoleColor background, bool damageElements, bool damageBackground, float elementsPercent, char elementsChar, List<ConsoleColor> colorsElements)
+        // Constructor de clase. Genera de forma random algunos elementos en el carril
         {
             this.posY = posY;
-            this.speedElements = Utils.rnd.Next(-10, 10) < 0 ? 1 : -1;
-            this.speedPlayer = speedPlayer;
-            this.background = background;
-            this.damageElements = damageElements;
-            this.damageBackground = damageBackground;
+            this.speedElements = Utils.rnd.Next(-10, 10) < 0 ? 1 : -1;  
+            this.speedPlayer = speedPlayer; 
+            this.background = background;   
+            this.damageElements = damageElements;   
+            this.damageBackground = damageBackground;   
 
             for (int i = 0; i < Utils.MAP_WIDTH; i++)
             {
@@ -41,6 +42,7 @@ namespace M05_UF3_P3_Frogger
         }
 
         public void Draw()
+        // Dibuja el carril
         {
             Console.SetCursorPosition(0, posY);
             Console.BackgroundColor = background;
@@ -59,6 +61,7 @@ namespace M05_UF3_P3_Frogger
             }
         }
         public void Update()
+        // Actualiza todos los elementos
         {
             foreach (DynamicElement element in elements)
             {
@@ -67,6 +70,7 @@ namespace M05_UF3_P3_Frogger
         }
 
         public DynamicElement ElementAtPosition(Vector2Int position)
+        // Busca un elemento en una posicion en el carril y devuelve ese elemento
         {
             foreach (DynamicElement element in elements)
             {
--- M05_UF3_P3_Frogger/Program.cs
+++ M05_UF3_P3_Frogger/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace M05_UF3_P3_Frogger
 {
@@ -8,7 +10,43 @@ namespace M05_UF3_P3_Frogger
     {
         static void Main(string[] args)
         {
-            //TODO
+            List<Lane> lineas = new List<Lane>();
+            
+            lineas.Add(new Lane(1, false, ConsoleColor.Green, false, false, 0, ' ', new List<ConsoleColor> {ConsoleColor.Green}));
+            lineas.Add(new Lane(2, true, ConsoleColor.Blue, false, true, 0, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lineas.Add(new Lane(3, true, ConsoleColor.Blue, false, true, 0, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lineas.Add(new Lane(4, true, ConsoleColor.Blue, false, true, 0, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lineas.Add(new Lane(5, true, ConsoleColor.Blue, false, true, 0, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lineas.Add(new Lane(6, true, ConsoleColor.Blue, false, true, 0, Utils.charLogs, new List<ConsoleColor>(Utils.colorsLogs)));
+            lineas.Add(new Lane(7, false, ConsoleColor.Green, false, false, 0, ' ', new List<ConsoleColor> {ConsoleColor.Green}));
+            lineas.Add(new Lane(8, false, ConsoleColor.Black, true, false, 0, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lineas.Add(new Lane(9, false, ConsoleColor.Black, true, false, 0, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lineas.Add(new Lane(10, false, ConsoleColor.Black, true, false, 0, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lineas.Add(new Lane(11, false, ConsoleColor.Black, true, false, 0, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lineas.Add(new Lane(12, false, ConsoleColor.Black, true, false, 0, Utils.charCars, new List<ConsoleColor>(Utils.colorsCars)));
+            lineas.Add(new Lane(13, false, ConsoleColor.Green, false, false, 0, ' ', new List<ConsoleColor> {ConsoleColor.Green}));
+
+         
+
+            while (true)
+            {
+                Input();
+                Logic();
+                foreach (Lane map in lineas)
+                {
+                    map.Draw();
+                }
+            }
+
+            void Input()
+            {
+                
+            }
+
+            void Logic()
+            {
+
+            }
         }
     }
 }
